260bcc874ad3b77edeac0ce53a8f3d76f9f3bae4
Aras Emami <aras.armani2014@gmail.com>
2025-05-24 12:05:16 +0330

    fixed short and long market visibility

--- src/App.tsx
+++ src/App.tsx
@@ -24,14 +24,27 @@ function App() {
   return (
 
     <div className=" w-h-screen flex items-center justify-center min-h-screen bg-gray-50 px-4 ">
-      <div className="bg-white p-8 rounded-lg shadow-md max-w-sm w-full text-center m-4">
-        <h1 className="text-2xl sm:text-lg font-extrabold text-gray-800">Market Status</h1>
+      <div className="bg-white p-4 rounded-lg shadow-md max-w-sm w-full text-center m-4">
+        <h3 className="text-xl sm:text-lg font-bold text-gray-800">Market Status</h3>
         <p className="mt-6 text-lg  text-gray-700">
           Market is:{" "}
           <span className={`font-bold ${sentiment === 'Long' ? 'text-green-600' : 'text-red-600'}`}>
             {sentiment}
           </span>
         </p>
+
+        <div className="flex items-center gap-2 bg-gray-50 p-4 rounded-lg shadow-sm mb-2">
+          <span className="font-semibold text-gray-700">Short:</span>
+          <span className="text-green-600 font-medium bg-green-100 px-2 py-1 rounded">TP: 20%</span>
+          <span className="text-red-600 font-medium bg-red-100 px-2 py-1 rounded">SL: 80%</span>
+        </div>
+
+        <div className="flex items-center gap-2 bg-gray-50 p-4 rounded-lg shadow-sm">
+          <span className="font-semibold text-gray-700">Long:</span>
+          <span className="text-green-600 font-medium bg-green-100 px-2 py-1 rounded">TP: 80%</span>
+          <span className="text-red-600 font-medium bg-red-100 px-2 py-1 rounded">SL: 20%</span>
+        </div>
+
       </div>
     </div>
 
