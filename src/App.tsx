35e55f46401381888734ce50ea269e3d667c8223
Aras Emami <aras.armani2014@gmail.com>
2025-05-25 12:44:11 +0330

    fixed

--- src/App.tsx
+++ src/App.tsx
@@ -25,24 +25,29 @@ function App() {
 
     <div className=" w-h-screen flex items-center justify-center min-h-screen bg-gray-50 px-4 ">
       <div className="bg-white p-4 rounded-lg shadow-md max-w-sm w-full text-center m-4">
-        <h3 className="text-xl sm:text-lg font-bold text-gray-800">Market Status</h3>
-        <p className="mt-6 text-lg  text-gray-700">
-          Market is:{" "}
-          <span className={`font-bold ${sentiment === 'Long' ? 'text-green-600' : 'text-red-600'}`}>
-            {sentiment}
-          </span>
-        </p>
+        <h3 className="text-xl sm:text-lg font-bold text-gray-800 text-left pb-4">Market Status</h3>
 
-        <div className="flex items-center gap-2 bg-gray-50 p-4 rounded-lg shadow-sm mb-2">
-          <span className="font-semibold text-gray-700">Short:</span>
-          <span className="text-green-600 font-medium bg-green-100 px-2 py-1 rounded">TP: 20%</span>
-          <span className="text-red-600 font-medium bg-red-100 px-2 py-1 rounded">SL: 80%</span>
-        </div>
+        <div className='w-full flex flex-col gap-2'>
+          <div className="flex items-center justify-center gap-2 bg-gray-50 p-4 rounded-lg shadow-sm">
+            <p className="text-lg  text-gray-700">
+              Market is:{" "}
+              <span className={`font-bold ${sentiment === 'Long' ? 'text-green-600' : 'text-red-600'}`}>
+                {sentiment}
+              </span>
+            </p>
+          </div>
 
-        <div className="flex items-center gap-2 bg-gray-50 p-4 rounded-lg shadow-sm">
-          <span className="font-semibold text-gray-700">Long:</span>
-          <span className="text-green-600 font-medium bg-green-100 px-2 py-1 rounded">TP: 80%</span>
-          <span className="text-red-600 font-medium bg-red-100 px-2 py-1 rounded">SL: 20%</span>
+          <div className="flex items-center gap-2 bg-gray-50 p-4 rounded-lg shadow-sm">
+            <span className="font-semibold text-gray-700">Short:</span>
+            <span className="text-green-600 font-medium bg-green-100 px-2 py-1 rounded">TP: 20%</span>
+            <span className="text-red-600 font-medium bg-red-100 px-2 py-1 rounded">SL: 80%</span>
+          </div>
+
+          <div className="flex items-center gap-2 bg-gray-50 p-4 rounded-lg shadow-sm">
+            <span className="font-semibold text-gray-700">Long:</span>
+            <span className="text-green-600 font-medium bg-green-100 px-2 py-1 rounded">TP: 80%</span>
+            <span className="text-red-600 font-medium bg-red-100 px-2 py-1 rounded">SL: 20%</span>
+          </div>
         </div>
 
       </div>
